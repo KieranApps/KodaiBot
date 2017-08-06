c0459ce2fc25d55fc1ab3b21b7b2b7d3ea8290a4
Giovanni Paul <giovannip@delta-n.nl>
2017-08-06 17:50:51 +0200

    Updated version

---
+++ KodaiBot/KodaiBot.Common/ConfigurationModel/Configurator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodaiBot.Common.ConfigurationModel
+{
+    public class Configurator
+    {
+        public class Bot
+        {
+            public static string Name
+                => GetAppSettingValue(Constants.Bot.Name);
+
+            public static string Url
+                => GetAppSettingValue(Constants.Bot.Url);
+
+            public static string Prefix
+                => GetAppSettingValue(Constants.Bot.Prefix);
+
+            public static string Token
+                => GetAppSettingValue(Constants.Bot.Token);
+        }
+
+        public class Database
+        {
+            public static string Host
+                => GetAppSettingValue(Constants.Database.Host);
+
+            public static string Port
+                => GetAppSettingValue(Constants.Database.Port);
+
+            public static string User
+                => GetAppSettingValue(Constants.Database.User);
+
+            public static string Password
+                => GetAppSettingValue(Constants.Database.Password);
+        }
+
+        private static string GetAppSettingValue(string key)
+        {
+            var val = ConfigurationManager.AppSettings[key];
+
+            if (val == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return val;
+        }
+    }
+}
---
+++ KodaiBot/KodaiBot.Common/ConfigurationModel/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodaiBot.Common.ConfigurationModel
+{
+    public class Constants
+    {
+        public class Bot
+        {
+            public const string Name = "Bot.Name";
+            public const string Url = "Bot.Url";
+            public const string Prefix = "Bot.Prefix";
+            public const string Token = "Bot.Token";
+        }
+
+        public class Database
+        {
+            public const string Host = "Database.Host";
+            public const string Port = "Database.Port";
+            public const string User = "Database.User";
+            public const string Password = "Database.Password";
+        }
+    }
+}
